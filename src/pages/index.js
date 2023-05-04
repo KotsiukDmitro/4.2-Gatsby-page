4f942c2e460e573df90d3fa7e36c94e4e75d0aae
Dmitro Kotsiuk <kotsiukdmitro@gmail.com>
2023-05-04 16:38:47 +0300

    stage-1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,30 @@
 import * as React from 'react'
-import { useState, Fragment } from 'react'
+import { useState } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { GatsbyImage, StaticImage, getImage } from 'gatsby-plugin-image'
-import { Disclosure } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Dialog, Transition } from '@headlessui/react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination } from "swiper";
 import 'swiper/css'
 import 'swiper/css/navigation'
 import "swiper/css/pagination";
 import {
-    slider,
     carousel,
-    form,
-    sliderImg,
     carouselImg,
-    labelInput,
-    formItem,
-    btn,
-    formInput,
-
 
 } from '../components/layout.module.css'
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+import Accordion from 'react-bootstrap/Accordion';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 const IndexPage = () => {
@@ -45,168 +45,144 @@ const IndexPage = () => {
     function handleSubmit() {
         alert('Данные успешно отправлены')
     }
-    let [isOpen, setIsOpen] = useState(false)
-    function closeModal() {
-        setIsOpen(false)
-    }
-    function openModal() {
-        setIsOpen(true)
-    }
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
 
     return (
         <Layout pageTitle="Home Page">
-            <Swiper navigation={true} modules={[Navigation]} className={slider}>
-                {images.allFile.nodes.map(node => (
-                    <SwiperSlide key={node.id}> <GatsbyImage image={getImage(node.childImageSharp.gatsbyImageData)} alt='photo' className={sliderImg} /></SwiperSlide>
-                ))}
+            <Container>
 
-                {/* Статический вариант вывода изображений  */}
+                <Carousel className='mt-5 border border-primary rounded'>
+                    {images.allFile.nodes.map(node => (
+                        <Carousel.Item key={node.id}>
+                            <GatsbyImage image={getImage(node.childImageSharp.gatsbyImageData)} alt='slide' className="d-block w-100" />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
 
-                {/* // <SwiperSlide ><StaticImage src='../images/background/slide-1.avif' alt='logo' className={sliderImg}></StaticImage> </SwiperSlide>
-                // <SwiperSlide><StaticImage src='../images/background/slide-2.avif' alt='logo' className={sliderImg}></StaticImage> </SwiperSlide>
-                // <SwiperSlide><StaticImage src='../images/background/slide-3.avif' alt='logo' className={sliderImg}></StaticImage> </SwiperSlide> */}
-            </Swiper>
+                <Swiper className={carousel} navigation={true} modules={[Navigation, Pagination]} slidesPerView={3} pagination={true}>
 
-            <Swiper className={carousel} navigation={true} modules={[Navigation, Pagination]} slidesPerView={3} pagination={true}>
+                    <SwiperSlide><StaticImage src='../images/carousel/BMW.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/Lexus.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/Porsche.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/WV.jpg' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/Youtube.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/Youtube-logo.jpg' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                    <SwiperSlide><StaticImage src='../images/carousel/Instagram.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
+                </Swiper>
 
-                <SwiperSlide><StaticImage src='../images/carousel/BMW.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/Lexus.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/Porsche.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/WV.jpg' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/Youtube.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/Youtube-logo.jpg' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-                <SwiperSlide><StaticImage src='../images/carousel/Instagram.png' alt='logo' className={carouselImg}></StaticImage></SwiperSlide>
-            </Swiper>
+                <Accordion className=' mt-5 '>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Какая версия Tailwind CSS используется?1</Accordion.Header>
+                        <Accordion.Body>
+                            Все в пользовательском интерфейсе Tailwind спроектировано и разработано для последней версии Tailwind CSS, которая в настоящее время называется Tailwind CSS v 3.3 .
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Какие браузеры поддерживаются?</Accordion.Header>
+                        <Accordion.Body>
+                            Компоненты пользовательского интерфейса Tailwind предназначены для работы в последних стабильных версиях всех основных браузеров, включая Chrome, Firefox, Safari и Edge.
+                            Мы не поддерживаем Internet Explorer 11.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
 
-            <div className="w-full px-4 pt-16">
-                <div className="mx-auto w-full max-w-[600px] rounded-2xl bg-white p-2">
-                    <Disclosure>
-                        {({ open }) => (
-                            <>
-                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                                    <span>Какая версия Tailwind CSS используется?</span>
-                                    <ChevronDownIcon
-                                        className={`${open ? 'rotate-180 transform' : ''
-                                            } h-5 w-5 text-purple-500`}
-                                    />
-                                </Disclosure.Button>
-                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                    Все в пользовательском интерфейсе Tailwind спроектировано и разработано для последней версии Tailwind CSS, которая в настоящее время называется Tailwind CSS v 3.3 .
-                                </Disclosure.Panel>
-                            </>
-                        )}
-                    </Disclosure>
-                    <Disclosure as="div" className="mt-2">
-                        {({ open }) => (
-                            <>
-                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                                    <span>Какие браузеры поддерживаются?</span>
-                                    <ChevronDownIcon
-                                        className={`${open ? 'rotate-180 transform' : ''
-                                            } h-5 w-5 text-purple-500`}
-                                    />
-                                </Disclosure.Button>
-                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                    Компоненты пользовательского интерфейса Tailwind предназначены для работы в последних стабильных версиях всех основных браузеров, включая Chrome, Firefox, Safari и Edge.
-                                    Мы не поддерживаем Internet Explorer 11.
-                                </Disclosure.Panel>
-                            </>
-                        )}
-                    </Disclosure>
-                </div>
-            </div>
-
-            <form className={form} onSubmit={handleSubmit}>
-                <div className={formItem}>
-                    <label className={labelInput}>*Имя:</label>
-                    <input id='firstName' type='text' className={formInput} required></input>
-                </div>
-                <div className={formItem}>
-                    <label className={labelInput}>Возраст:</label>
-                    <input id='age' type='number' min='16' className={formInput}></input>
-                </div>
-                <div className={formItem}>
-                    <label className={labelInput}>Пол:</label>
-                    <select id='select'>
-                        <option value='male'>Мужской</option>
-                        <option value='famale'>Женский</option>
-                    </select>
-                </div>
-                <div className={formItem}>
-                    <label className={labelInput}> Хобби:</label>
-                    <label><input type="checkbox" id='sport' name='sport' value='sport'></input>Спорт</label>
-                    <label><input type="checkbox" id='music' name='music' value='music'></input>Музыка</label>
-                    <label><input type="checkbox" id='travel' name='travel' value='travel' ></input>Путишествие</label>
-                </div>
-
-                <input type='submit' value='отправить' className={btn}></input>
-            </form>
-
-            <div className=" inset-0 flex items-center mt-[50px] ml-[15%] mr-[15%] mb-[50px]">
-                <p> Lorem ipsum – классический вариант условного бессодержательного текста, вставляемого в макет страницы.
-                    <button
-                        type="button"
-                        onClick={openModal}
-                        className="rounded-md px-4 py-2 text-sm font-medium text-red-500"
-                    >
-                        Читать полностью ...
-                    </button>
-                </p>
-            </div>
-
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
+                <Form className='mt-5  mb-5 border border-primary rounded p-3 bg-light' onSubmit={handleSubmit}>
+                    <Row className="g-2">
+                        <Col md>
+                            <FloatingLabel
+                                controlId="floatingName"
+                                label="*Имя">
+                                <Form.Control
+                                    type="text"
+                                    id="firstName"
+                                    required
+                                    className='mb-3'
+                                />
+                            </FloatingLabel>
+                        </Col>
+                        <Col md>
+                            <FloatingLabel
+                                controlId="floatingNumber"
+                                label="Возраст"
                             >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
-                                    >
-                                        Lorem ipsum
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Lorem ipsum – классический вариант условного бессодержательного текста, вставляемого в макет страницы. Lorem ipsum – это извращенный отрывок из философского трактата Цицерона «О пределах добра и зла», написанного в 45 году до нашей эры по-латыни.
-                                        </p>
-                                    </div>
-
-                                    <div className="mt-4">
-                                        <button
-                                            type="button"
-                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                            onClick={closeModal}
-                                        >
-                                            Закрыть окно
-                                        </button>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
+                                <Form.Control
+                                    type="number"
+                                    id="age"
+                                    className='mb-3'
+                                    min='18'
+                                />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Row className="g-2">
+                        <Col md>
+                            <FloatingLabel
+                                controlId="floatingGender"
+                                label="Пол"
+                            >
+                                <Form.Select className='mb-3'>
+                                    <option>Мужской</option>
+                                    <option>Женский</option>
+                                </Form.Select>
+                            </FloatingLabel>
+                        </Col>
+                        <Col md>
+                            <FloatingLabel
+                                controlId="floatingEmail"
+                                label="Email">
+                                <Form.Control
+                                    type="email"
+                                    id="email"
+                                    required
+                                    className='mb-3'
+                                />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Form.Label>Хобби:</Form.Label>
+                    <div className="mb-3">
+                        <Form.Check
+                            inline
+                            label="спорт"
+                            name="group1"
+                            type='checkbox'
+                            id='sport'
+                        />
+                        <Form.Check
+                            inline
+                            label="музыка"
+                            name="music"
+                            type="checkbox"
+                            id="music"
+                        />
+                        <Form.Check
+                            inline
+                            label="путешествие"
+                            name="travel"
+                            type='checkbox'
+                            id="travel"
+                        />
                     </div>
-                </Dialog>
-            </Transition>
+                    <Button as="input" type="submit" value="отправить" />
+
+                </Form>
+
+                <div className=" inset-0 flex items-center">
+                    <p> Lorem ipsum – классический вариант условного бессодержательного текста, вставляемого в макет страницы.
+                        <Button variant="primary" onClick={handleShow}>
+                            Читать полностью ...
+                        </Button>
+                    </p>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton></Modal.Header>
+                        <Modal.Body> Lorem ipsum – классический вариант условного бессодержательного текста, вставляемого в макет страницы. Lorem ipsum – это извращенный отрывок из философского трактата Цицерона «О пределах добра и зла», написанного в 45 году до нашей эры по-латыни.</Modal.Body>
+
+                    </Modal>
+                </div>
+            </Container>
         </Layout >
     )
 }
@@ -214,8 +190,6 @@ const IndexPage = () => {
 export const Head = () => <Seo title='Home Page' />
 
 export default IndexPage
-
-
 
 
 
